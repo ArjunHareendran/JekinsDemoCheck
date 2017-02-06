5496bc9f96a0c57965705f209d6549dd47bcd1c9
Arjun Hareendran <arjun.hareeendran@gmail.com>
2017-02-06 12:26:45 +0530

    Test Case Update

--- JenkinsDemo/src/test/java/CalculatorTest.java
+++ JenkinsDemo/src/test/java/CalculatorTest.java
@@ -12,4 +12,25 @@ public class CalculatorTest {
 		assertEquals(3, obj.sum(1, 2));
 		assertTrue(obj.sum(1, 2) != 4);		
 	}
+	
+	@Test
+	public void divTest() {
+		Calculator obj = new Calculator();
+		assertEquals(1, obj.div(2, 2));
+		assertTrue(obj.div(2, 2) != 2);		
+	}
+	
+	@Test
+	public void mulTest() {
+		Calculator obj = new Calculator();
+		assertEquals(4, obj.mul(2, 2));
+		assertTrue(obj.mul(2, 2) != 5);		
+	}
+	
+	@Test
+	public void subTest() {
+		Calculator obj = new Calculator();
+		assertEquals(0, obj.sub(2, 2));
+		assertTrue(obj.sub(2, 2) != 1);		
+	}
 }
